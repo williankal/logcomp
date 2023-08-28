6315da451cd79c8dabd87db35f8ede951cadb5ba
Willian Kenzo <kenzo.willian@hotmail.com>
2023-08-28 01:00:42 -0300

    teste2

--- main.py
+++ main.py
@@ -1,46 +1,88 @@
 import sys
 
-def ignora_espaco(string):
-    tira_espaco = string.replace(" ", "")
-    return tira_espaco
+class Token:
+    def __init__(self, type : str, value : int):
+        self.type = type
+        self.value = value
 
-def check_erros(string):
-    if len(string) == 0 or ('+' not in string and '-' not in string):
-        raise ValueError
+class Tokenizer:
+    def __init__(self, source: str, position : int = 0):
+        self.source = source
+        self.position = 0
+        self.next = Token
     
-    elif string[0] == '+' or string[0] == '-':   
-        raise ValueError
-    
-    else:
-        return 
+    def selectNext(self):
+        if self.position == len(self.source):
+            self.next = Token("EOF", " ")
 
-def operacao_aritmetica(string):
-    lista_soma = string.split('+')
-    lista_sub = []
-    valor = 0
-    for i in lista_soma:
-        if "-" in i:
-            lista_sub.append(i)
+        elif self.source[self.position] == " ":
+            self.position += 1
+            self.selectNext()
+            
+        elif self.source[self.position] == "+":
+            self.position += 1
+            self.next = Token("PLUS", self.source[self.position])
+            return self.next
+
+        elif self.source[self.position] == "-":
+            self.position += 1
+            self.next = Token("MINUS",  self.source[self.position])
+            return self.next
+
+
+        elif self.source[self.position].isnumeric():
+            numero_completo = self.source[self.position]
+            self.position += 1
+
+            while self.position <= len(self.source):
+                
+                if self.source[self.position].isnumeric():
+                    numero_completo += self.source[self.position]
+                    self.position += 1
+
+                else :
+                    self.next = Token("NUM", int(numero_completo))
+                    return self.next
+                
+            self.next = Token("NUM", int(numero_completo))
+            return self.next
+        
+        
         else:
-            valor += int(i)
+            raise Exception("aaa")
+        
+    
+        
+class Parser:
+    tokens: Tokenizer
 
-    if len(lista_sub) > 0:
-        for i in lista_sub:
-            string_subs = i.split('-')
-            for index, value in enumerate(string_subs):
-                if index == 0:
-                    valor += int(value)
-                else:
-                    valor -= int(value)          
-    return valor
+    def parseExpression():
+        Parser.tokens.selectNext()
+        resultado = 0
 
-recebido = sys.argv[1]
+        while Parser.tokens.next.type != "EOF":
+            if Parser.tokens.next.type == "NUM":
+                resultado = Parser.tokens.next.value
+                Parser.tokens.selectNext()
 
-recebido_sem = ignora_espaco(recebido)
-check_erros(recebido_sem)
-resultado = operacao_aritmetica(recebido_sem)
-print(resultado)
+                while Parser.tokens.next.type == "PLUS" or Parser.tokens.next.type == "MINUS":
+                    if Parser.tokens.next.type == "PLUS":
+                        Parser.tokens.selectNext()
+                        if Parser.tokens.next.type == "NUM":
+                            resultado += Parser.tokens.next.value()
+
+                    elif Parser.tokens.next.type == "MINUS":
+                        Parser.tokens.selectNext()
+                        if Parser.tokens.next.type == "NUM":
+                            resultado -= Parser.tokens.next.value()
+                    Parser.tokens.selectNext()
+
+        print(resultado)
+        return resultado    
+
+    def run(code):
+        Parser.tokens = Tokenizer(code)
+        Parser.parseExpression()
 
 
-
-
+Parser.run(sys.argv[1])
