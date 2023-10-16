81af4323d89da067eb14fa34dfd2de97a6e29c61
Willian Kenzo <kenzo.willian@hotmail.com>
2023-10-15 22:10:03 -0300

    bug

--- code.go
+++ code.go
@@ -1,11 +1,6 @@
-Println(1)
-x = 3+6/3   *  2 -+-  +  2*4/2 + 0/1 -((6+ ((4)))/(2)) // Teste // Teste 2
-y_1 = 3
-y_1 = y_1 + 2
-
-z__ = x + y_1
-
-
-// Saida final
-Println(x)
-Println(z__+1)
+i = 10
+if  i < 10 {
+	Println(10)
+} else {
+	Println(2)
+}
--- main.py
+++ main.py
@@ -15,7 +15,10 @@ class PrePro:
           
     def filter(self):
         self.pre_string = re.sub('//.*', "", self.pre_string)
-        return self.pre_string.strip()
+        
+        lines = self.pre_string.split('\n')
+        self.pre_string = '\n'.join([line.lstrip('\t') for line in lines])
+        return self.pre_string  
     
 
 class Node:
@@ -24,7 +27,7 @@ class Node:
         self.children = children
         
     @abstractmethod
-    def evaluate(self, table: SymbolTable):
+    def Evaluate(self, table: SymbolTable):
         pass
 
 class BinOp(Node):
@@ -37,6 +40,22 @@ class BinOp(Node):
             return self.children[0].Evaluate(table) * self.children[1].Evaluate(table)
         elif self.value == "/":
             return self.children[0].Evaluate(table) // self.children[1].Evaluate(table)
+        elif self.value == "==":
+            return self.children[0].Evaluate(table) == self.children[1].Evaluate(table)
+        elif self.value == "<":
+            return self.children[0].Evaluate(table) < self.children[1].Evaluate(table)
+        elif self.value == ">":    
+            return self.children[0].Evaluate(table) > self.children[1].Evaluate(table)
+        elif self.value == "&&":
+            return self.children[0].Evaluate(table) and self.children[1].Evaluate(table)
+        elif self.value == "||":
+            return self.children[0].Evaluate(table) or self.children[1].Evaluate(table)
+        elif self.value == "!=":
+            return self.children[0].Evaluate(table) != self.children[1].Evaluate(table)
+        elif self.value == ">=":
+            return self.children[0].Evaluate(table) >= self.children[1].Evaluate(table)
+        elif self.value == "<=":
+            return self.children[0].Evaluate(table) <= self.children[1].Evaluate(table)
         
         else: 
             raise ValueError("BinOP Value error")
@@ -47,14 +66,19 @@ class UnOp(Node):
             return self.children[0].Evaluate(table)
         elif self.value == "-":
             return -self.children[0].Evaluate(table)
+        elif self.value == "!":
+            return not self.children[0].Evaluate(table)
         else: 
             raise ValueError("UnOP Value error")
+        
         
 class IntVal(Node):
     def Evaluate(self,table : SymbolTable):
         return self.value
 
 class NoOp(Node):
+    def __init__(self):
+        super().__init__(value=None, children=[]) 
     def Evaluate(self,table : SymbolTable):
         pass
 
@@ -63,12 +87,36 @@ class Identifier(Node):
     return table.getter(self.value)
 
 class Assignment(Node):
-  def Evaluate(self, table : SymbolTable):
-    table.setter(self.children[0].value, self.children[1].Evaluate(table))
+    def __init__(self, children, value=None):
+        super().__init__(value, children)
 
+    def Evaluate(self, table : SymbolTable):
+        table.setter(self.children[0], self.children[1].Evaluate(table))
+
+class Scanln(Node):
+    def __init__(self, children = None, value=None):
+        super().__init__(value, children)
+    def Evaluate(self, table : SymbolTable):
+        return int(input())
+    
 class Println(Node):
     def Evaluate(self, table : SymbolTable):
         print(self.children[0].Evaluate(table))
+
+class For(Node):
+    def Evaluate(self, table : SymbolTable):
+        self.children[0].Evaluate(table)
+        while self.children[1].Evaluate(table):
+            self.children[3].Evaluate(table)
+            self.children[2].Evaluate(table)
+
+class If(Node):
+    def Evaluate(self, table : SymbolTable):
+        if self.children[0].Evaluate(table):
+            self.children[1].Evaluate(table)
+        elif len(self.children) == 3:
+            self.children[2].Evaluate(table)
+
 class Block(Node):
   def Evaluate(self, table : SymbolTable):
     for child in self.children:
@@ -82,13 +130,18 @@ class Tokenizer:
         self.source = source
         self.position = 0
         self.next = None
-        self.reserved_words = ["Println"]
+        self.reserved_words = {"Println" : "Println", "if" : "if", "else" : "else", "for" : "for", "Scanln" : "Scanln"}
 
     def selectNext(self):
+
+
         if self.position >= len(self.source):
             self.next = Token("EOF", " ")
             return self.next
         
+        elif self.source[self.position] == " ":
+            self.position += 1
+            self.selectNext()
 
         elif self.source[self.position].isnumeric():
             num = self.source[self.position]
@@ -102,6 +155,11 @@ class Tokenizer:
                     self.next = Token("NUM", int(num))
                     return self.next
             self.next = Token("NUM", int(num))
+            return self.next
+        
+        elif self.source[self.position] == ";":
+            self.position += 1
+            self.next = Token("SEMICOLON", " ")
             return self.next
 
         elif self.source[self.position] == "+":
@@ -125,9 +183,6 @@ class Tokenizer:
             return self.next
         
 
-        elif self.source[self.position] == " ":
-            self.position += 1
-            self.selectNext()
 
         elif self.source[self.position] == "(":
             self.position += 1
@@ -141,12 +196,75 @@ class Tokenizer:
         
         elif self.source[self.position] == "=":
             self.position += 1
+            if self.source[self.position] == "=":
+                self.position += 1
+                self.next = Token("EQUAL_EQUAL", " ")
+                return self.next
+            
             self.next = Token("EQUAL", " ")
             return self.next
         
         elif self.source[self.position] == "\n":
             self.position += 1
             self.next = Token("ENTER", " ")
+            return self.next
+        
+        elif self.source[self.position] == "<":
+            self.position += 1
+            self.next = Token("LESS", " ")
+            return self.next
+        
+        elif self.source[self.position] == ">":
+            self.position += 1
+            self.next = Token("GREATER", " ")
+            return self.next
+        
+        elif self.source[self.position] == ">=":
+            self.position += 1
+            self.next = Token("GREATER_EQUAL", " ")
+            return self.next
+        
+        elif self.source[self.position] == "<=":
+            self.position += 1
+            self.next = Token("LESS_EQUAL", " ")
+            return self.next
+        
+        elif self.source[self.position] == "!=":
+            self.position += 1
+            self.next = Token("NOT_EQUAL", " ")
+            return self.next
+
+        elif self.source[self.position] == "!":
+            self.position += 1
+            self.next = Token("NOT", " ")
+            return self.next
+        
+        elif self.source[self.position] == "&":
+            self.position += 1
+            if self.source[self.position] == "&":
+                self.position += 1
+                self.next = Token("AND", " ")
+                return self.next
+            else: 
+                raise ValueError("& not found")
+        
+        elif self.source[self.position] == "|" :
+            self.position += 1
+            if self.source[self.position] == "|":
+                self.position += 1
+                self.next = Token("OR", " ")
+                return self.next
+            else:
+                raise ValueError("| not found")
+            
+        elif self.source[self.position] == "{":
+            self.position += 1
+            self.next = Token("OPEN_BRACES", " ")
+            return self.next
+        
+        elif self.source[self.position] == "}":
+            self.position += 1
+            self.next = Token("CLOSE_BRACES", " ")
             return self.next
         
         elif self.source[self.position].isalpha() or self.source[self.position] == "_":
@@ -159,16 +277,23 @@ class Tokenizer:
                 variable += self.source[self.position]
                 self.position += 1
 
-            # Check if the variable is a reserved word
-            type = "PRINTLN" if variable in self.reserved_words else "IDENTIFIER"
-            self.next = Token(type, variable)
-            return self.next
+            if variable in self.reserved_words:
+                self.next = Token(self.reserved_words[variable], None)
+
+            else:
+                self.next = Token("IDENTIFIER", variable)
+        
 
         else:
-            raise Exception("Invalid char")
-
+            raise ValueError("Invalid string")
 class Parser:
     tokens: None
+
+    def parseProgram():
+        children = []
+        while Parser.tokens.next.type != "EOF":
+            children.append(Parser.parseStatement())
+        return children
 
     def parseFactor():
        if Parser.tokens.next.type == "NUM":
@@ -183,10 +308,14 @@ class Parser:
        elif Parser.tokens.next.type == "MINUS":
            Parser.tokens.selectNext()
            node = UnOp("-" , [Parser.parseFactor()])
+
+       elif Parser.tokens.next.type == "NOT":
+            Parser.tokens.selectNext()
+            node = UnOp("!", [Parser.parseFactor()])
       
        elif Parser.tokens.next.type == "OPEN_PAREN":
             Parser.tokens.selectNext()
-            node = Parser.parseExpression()
+            node = Parser.parserBoolExpression()
             if Parser.tokens.next.type != "CLOSE_PAREN":
                raise ValueError("Invalid string")
 
@@ -197,12 +326,23 @@ class Parser:
             node = Identifier(Parser.tokens.next.value, [])
             Parser.tokens.selectNext()
 
+       elif Parser.tokens.next.type == "Scanln":
+            Parser.tokens.selectNext()
+            if Parser.tokens.next.type == "OPEN_PAREN":
+                Parser.tokens.selectNext()
+                node = Scanln()
+                if Parser.tokens.next.type == "CLOSE_PAREN":
+                    Parser.tokens.selectNext()
+                else:
+                    raise ValueError("Invalid string")
+            else:
+                raise ValueError("Invalid string")
+
        else:
             raise ValueError("Invalid string")
       
        return node
     
-
     def parserTerm():
 
         node = Parser.parseFactor()
@@ -218,14 +358,64 @@ class Parser:
                 node = BinOp("*", [node, Parser.parseFactor()])
 
         return node
+    
+    def parserBoolExpression():
+        node = Parser.parseBooTerm()
+        while (Parser.tokens.next.type == "OR"):
+            Parser.tokens.selectNext()
+            node = BinOp("||", [node, Parser.parseBooTerm()])
+        return node
+    
+    def parseBooTerm():
+        node = Parser.relationExpression()
+        while (Parser.tokens.next.type == "AND"):
+            Parser.tokens.selectNext()
+            node = BinOp("&&", [node, Parser.relationExpression()])
+        return node
+    
+    def relationExpression():
+        node = Parser.parseExpression()
+
+        if Parser.tokens.next.type == "EQUAL_EQUAL":
+            Parser.tokens.selectNext()
+            node = BinOp("==", [node, Parser.parseExpression()])
+        elif Parser.tokens.next.type == "LESS":
+            Parser.tokens.selectNext()
+            node = BinOp("<", [node, Parser.parseExpression()])
+        elif Parser.tokens.next.type == "GREATER":
+            Parser.tokens.selectNext()
+            node = BinOp(">", [node, Parser.parseExpression()])
+
+        elif Parser.tokens.next.type == "NOT_EQUAL":
+            Parser.tokens.selectNext()
+            node = BinOp("!=", [node, Parser.parseExpression()])
+
+        elif Parser.tokens.next.type == "LESS_EQUAL":
+            Parser.tokens.selectNext()
+            node = BinOp("<=", [node, Parser.parseExpression()])
+
+        elif Parser.tokens.next.type == "GREATER_EQUAL":
+            Parser.tokens.selectNext()
+            node = BinOp(">=", [node, Parser.parseExpression()])
+        
+        return node
                 
     
     def parseBlock():
-        children = []
-        while Parser.tokens.next.type != "EOF":
-            children.append(Parser.parseStatement())
-                
-        return children
+        if Parser.tokens.next.type == "OPEN_BRACES":
+            Parser.tokens.selectNext()
+            if Parser.tokens.next.type == "ENTER":
+                Parser.tokens.selectNext()
+                root = Parser.parseStatement()
+            else:
+                raise ValueError("sem enter")
+            if Parser.tokens.next.type == "CLOSE_BRACES":
+                Parser.tokens.selectNext()
+                return root
+            else:
+                raise ValueError("sem chaves fechadas")
+        else: 
+            raise ValueError("sem chaves abertas")
 
         
     def parseExpression():
@@ -246,60 +436,98 @@ class Parser:
                 raise ValueError
         return node
     
+
+    def parseProgram():
+        children = []
+        while Parser.tokens.next.type != "EOF":
+            children.append(Parser.parseStatement())
+        return children
+    
     def parseStatement():
-        if Parser.tokens.next.type == "IDENTIFIER":
-            # Parse assignment statement
-            identifier = Identifier(Parser.tokens.next.value, [])
+        root = NoOp()
+        if Parser.tokens.next.type == "Println":
+            Parser.tokens.selectNext()
+            if Parser.tokens.next.type == "OPEN_PAREN":
+                Parser.tokens.selectNext()
+                raiz_print = Parser.parserBoolExpression()
+                if Parser.tokens.next.type == "CLOSE_PAREN":
+                    Parser.tokens.selectNext()
+                    root = Println("Println", [raiz_print])
+                else:
+                    raise ValueError("Invalid string")
+                
+            else:
+                raise ValueError("Invalid string")
+            
+        elif Parser.tokens.next.type == "if":
+
+            Parser.tokens.selectNext()
+            raiz_if = Parser.parserBoolExpression()
+            raiz_block = Parser.parseBlock()
+            if Parser.tokens.next.type == "else":
+                Parser.tokens.selectNext()
+                raiz_else = Parser.parseBlock()
+                root = If("if", [raiz_if, raiz_block, raiz_else])
+            else:
+                root = If("if", [raiz_if, raiz_block])
+
+        elif Parser.tokens.next.type == "for":
+            Parser.tokens.selectNext()
+            root_init = Parser.tokens.next.value
             Parser.tokens.selectNext()
 
             if Parser.tokens.next.type == "EQUAL":
                 Parser.tokens.selectNext()
-                expression = Parser.parseExpression()
-                node = Assignment("=", [identifier, expression])
-            else:
-                raise Exception("Parse statement error: Expected '=' after identifier")
-
-        elif Parser.tokens.next.type == "PRINTLN":
-            # Parse printf statement
-            Parser.tokens.selectNext()
-
-            if Parser.tokens.next.type == "OPEN_PAREN":
-                Parser.tokens.selectNext()
-                expression = Parser.parseExpression()
-                node = Println("PRINTLN", [expression])
-
-                if Parser.tokens.next.type == "CLOSE_PAREN":
+                root_init = Assignment (children=[root_init, Parser.parserBoolExpression()])
+                if Parser.tokens.next.type == "SEMICOLON":
                     Parser.tokens.selectNext()
-
+                    raiz_cond = Parser.parserBoolExpression()
+                    if Parser.tokens.next.type == "SEMICOLON":
+                        Parser.tokens.selectNext()
+                        if Parser.tokens.next.type == "IDENTIFIER":
+                            raiz_inc = Parser.tokens.next.value
+                            Parser.tokens.selectNext()
+                            if Parser.tokens.next.type == "EQUAL":
+                                Parser.tokens.selectNext()
+                                raiz_inc = Assignment(children=[raiz_inc, Parser.parseExpression()])
+                                raiz_block = Parser.parseBlock()
+                                root = For("for", [root_init, raiz_cond, raiz_inc, raiz_block])
+                            else:
+                                raise ValueError("Invalid string")
+                        else:
+                            raise ValueError("Invalid string")
+                    else:
+                        raise ValueError("Invalid string")
                 else:
-                    raise Exception("Parse statement error: Missing closing parenthesis")
+                    raise ValueError("Invalid string")
             else:
-                raise Exception("Parse statement error: Missing open parenthesis after printf")
-        
-        elif Parser.tokens.next.type == "ENTER" or Parser.tokens.next.type == "EOF":
+                raise ValueError("Invalid string")
+            
+        elif Parser.tokens.next.type == "IDENTIFIER":
+            raiz_id = Parser.tokens.next.value
             Parser.tokens.selectNext()
-            node = NoOp("NOOP", [])
-            return node
+            if Parser.tokens.next.type == "EQUAL":
+                Parser.tokens.selectNext()
+                root = Assignment("EQUAL", [raiz_id, Parser.parserBoolExpression()])
 
-        else:
-            raise Exception("Parse statement error: Unexpected token")
-        
+            else:
+                raise ValueError("Invalid string")
+            
+        if Parser.tokens.next.type in ["ENTER", "EOF"]:
+            Parser.tokens.selectNext()
+            return root
 
-        return node
+            
 
     def run(arquivo):
         expressao_semcoment = PrePro(arquivo).filter()
         table = SymbolTable()
         Parser.tokens = Tokenizer(expressao_semcoment)  
         Parser.tokens.selectNext()
-        node1 = Parser.parseBlock()
-        
-        if Parser.tokens.next.type != "EOF":
-            raise Exception("Invalid string")
-        else:
-            # print(node1.Evaluate(table))
-            for node in node1:
-                node.Evaluate(table)
+
+        for root in Parser.parseProgram():
+
+            root.Evaluate(table)
 
 
 expressao = open(sys.argv[1], "r")
